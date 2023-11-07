2ad55f829e23764a57efb9d30d1d4c5baddfaa5c
Christopher Smith <81260517+vecto-solutions@users.noreply.github.com>
2023-11-07 08:56:47 -0500

    Update index.js
    Initial Update

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,14 @@ export default function Home() {
       {/* left side */}
       <div className="lg:w-1/2 relative z-10 flex flex-col justify-center px-10 lg:px-20 py-20 lg:py-0 text-left">
         <h2 className="inter text-4xl mb-3 font-bold text-gray-800">
-          Welcome to Your Next.js App{" "}
+          This is a minimum Next.js App, that is Deploying Next.js as a Custom Server{" "}
           <span className="block text-blue-500 text-2xl font-normal">
             Deployed to DigitalOcean
           </span>
         </h2>
+            <h2>
+            Testing by Vecto Solutions
+            </h2>
 
         <p className="text-gray-700 mb-6">
           Deploy API routes, static frontend, databases, and more.
@@ -21,11 +24,11 @@ export default function Home() {
           >
             View the Docs
           </a>
-          <a
-            href="https://cloud.digitalocean.com/apps"
-            className="block py-2 px-5 rounded shadow bg-blue-500 text-blue-100"
+              <a
+            href="https://docs.digitalocean.com/developer-center/deploy-a-next.js-app-to-app-platform/"
+            className="block py-2 px-5 rounded shadow bg-gray-500 text-gray-100 sm:mr-2 mb-2 sm:mb-0"
           >
-            View Your Dashboard
+            DEPLOY next.js to DO App Platform
           </a>
         </div>
       </div>
